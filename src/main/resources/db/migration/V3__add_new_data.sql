d5b21b240b1a8802f3a4017ad1bd5c99800bff21
sramprakashs <64207576+sramprakashs@users.noreply.github.com>
2020-07-28 12:56:16 +0530

    Update V3__add_new_data.sql

--- src/main/resources/db/migration/V3__add_new_data.sql
+++ src/main/resources/db/migration/V3__add_new_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO employee (name) VALUES('Ramp');
 INSERT INTO employee (name) VALUES('Prakash');
-NSERT INTO employee (name) VALUES('Ramprakash');
+INSERT INTO employee (name) VALUES('Ramprakash');
